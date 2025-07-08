02bf8f0f81d6f039d1f2f44bff037e5aea218d56
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2025-07-08 22:57:35 +0700

    fix: midtrans notification

--- controller/midtrans_controller_impl.go
+++ controller/midtrans_controller_impl.go
@@ -36,6 +36,7 @@ func (controller *MidtransControllerImpl) ListenNotification(c *gin.Context) {
 	var request model.MidtransNotification
 
 	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		helper.PanicIfError(err)
 	}
 
